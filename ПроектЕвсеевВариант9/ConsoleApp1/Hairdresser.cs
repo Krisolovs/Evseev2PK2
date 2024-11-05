61d298432923d65b6caaf5e13500138d266ed498
KRbISOLOV <150155555+Krisolovs@users.noreply.github.com>
2024-11-05 13:55:03 +0800

    Add files via upload

---
+++ ПроектЕвсеевВариант9/ConsoleApp1/Hairdresser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+        public class Hairdresser : Person
+        {
+            public string Specialization { get; set; }
+            public int Experience { get; set; } // В годах
+
+            public void PerformService(OrderedService orderedService)
+            {
+                Console.WriteLine($"{Name} выполняет услугу {orderedService.Service.Name} для клиента {orderedService.Client.Name}.");
+            }
+        }
+
+  
+}
---
+++ ПроектЕвсеевВариант9/ConsoleApp1/PriceList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class PriceList
+    {
+        public List<Service> Services { get; set; } = new List<Service>();
+
+        public void AddService(Service service)
+        {
+            Services.Add(service);
+        }
+
+        public void DisplayPriceList()
+        {
+            Console.WriteLine("Прайс-лист:");
+            foreach (var service in Services)
+            {
+                Console.WriteLine(service.GetServiceInfo());
+            }
+        }
+    }
+}
